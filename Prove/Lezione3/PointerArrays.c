7aaf25aa4e6f81d45daa34c07f41351cf17f1694
Daniele Moschini <daniele_moschini@hotmail.it>
2020-05-21 14:25:45 +0200

    [UPDATE] Prove

--- Prove/Lezione3/PointerArrays.c
+++ Prove/Lezione3/PointerArrays.c
@@ -11,17 +11,17 @@ int main(void)
         buffer[k] = k;
     }
     unsigned *pbuffer1 = buffer;
-    unsigned *pbuffer2 = buffer +5;
-    for ( int i = 0; i < dim; i++)
+    unsigned *pbuffer2 = buffer + 5;
+    for (int i = 0; i < dim; i++)
     {
-       printf("%d\t", buffer[i]);
+        printf("%d\t", buffer[i]);
     }
     printf("\n");
     for (int i = 5; i < dim; i++)
     {
         printf("%d\t", pbuffer2[i]);
     }
-    
+
     printf("\n");
     printf("\n");
 }
